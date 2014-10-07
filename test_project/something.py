20137af6fbd6985f8f6ab012ee7d6fc1fb07878d
mattiabs <mattia@knows.is>
2014-10-07 18:41:15 +0100

    End first day

--- test_project/something.py
+++ test_project/something.py
@@ -1,3 +1,48 @@
 __author__ = 'Mattia'
 
-print("hello")
+import sys
+import os
+import json
+import matplotlib.pyplot as plt
+
+
+#my_file = open("4f144ff15ccb6e3b0b00000e.json")
+
+for all_file in os.listdir(os.getcwd()):
+    if all_file.endswith(".json"):
+        my_file = open(all_file)
+
+        i            = 0
+        dates        = {}
+        time_windows = {}
+
+        # time_window in seconds
+        time_window_length = 100
+
+        index_baseline     = 1
+        #time_window_counts = 0
+        time_window_index  = 1
+
+        for line in my_file:
+            i += 1
+            dates[i] = json.loads(line).get("created_at").get("$date")
+
+            if dates[index_baseline] - dates[i] < time_window_length:
+                time_windows[time_window_index] = time_windows.get(time_window_index, 0) + 1
+            else:
+                time_window_index += 1
+                index_baseline     = i
+
+        print(len(time_windows.values()))
+        print(time_window_index)
+
+        fig, ax = plt.subplots()
+        ax.hist(dates.values(), 10, normed=1, histtype='stepfilled')
+        plt.show()
+
+        #x = range(1, time_window_index)
+        #plt.plot(x, time_windows.values())
+
+        print("end")
+
+print("end_end")
