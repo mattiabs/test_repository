4eb54a2cf09d4991a5b9032b1f7702ccdf8b5f41
mattiabs <mattia@knows.is>
2014-10-08 18:18:24 +0100

    Second day
    interArrival_times

--- test_project/something.py
+++ test_project/something.py
@@ -1,9 +1,16 @@
 __author__ = 'Mattia'
 
+from matplotlib import pyplot as plt
+
+
+#import matplotlib as mpl
+#mpl.use('Qt4Agg')
+
+
 import sys
 import os
 import json
-import matplotlib.pyplot as plt
+
 
 
 #my_file = open("4f144ff15ccb6e3b0b00000e.json")
@@ -15,6 +22,7 @@ for all_file in os.listdir(os.getcwd()):
         i            = 0
         dates        = {}
         time_windows = {}
+        interArrival_times = {}
 
         # time_window in seconds
         time_window_length = 100
@@ -25,23 +33,31 @@ for all_file in os.listdir(os.getcwd()):
 
         for line in my_file:
             i += 1
-            dates[i] = json.loads(line).get("created_at").get("$date")
+            dates[i] = json.loads(line).get("created_at").get("$date")/1000
 
-            if dates[index_baseline] - dates[i] < time_window_length:
-                time_windows[time_window_index] = time_windows.get(time_window_index, 0) + 1
-            else:
+            interArrival_times[i] = dates.get(i-1, dates.get(i)) - dates.get(i)
+
+            if dates[index_baseline] - dates[i] > time_window_length:
                 time_window_index += 1
                 index_baseline     = i
+
+            time_windows[time_window_index] = time_windows.get(time_window_index, 0) + 1
+
 
         print(len(time_windows.values()))
         print(time_window_index)
 
         fig, ax = plt.subplots()
-        ax.hist(dates.values(), 10, normed=1, histtype='stepfilled')
+        ax.hist(dates.values(), 100, histtype='stepfilled')
         plt.show()
 
         #x = range(1, time_window_index)
         #plt.plot(x, time_windows.values())
+
+        fig2, ax2 = plt.subplots()
+        ax2.hist(interArrival_times.values(), 100, histtype='stepfilled')
+        plt.show()
+
 
         print("end")
 
